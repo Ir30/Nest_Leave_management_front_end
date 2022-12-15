fff77c91606fe767444b32f7d9ed6a00fdecf8da
irshad007 <irshadmuhammedch8@gmail.com>
2022-12-15 18:54:14 +0530

    leave application

---
+++ src/app/leave-application/leave-application.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-application',
+  templateUrl: './leave-application.component.html',
+  styleUrls: ['./leave-application.component.css']
+})
+export class LeaveApplicationComponent {
+
+  
+
+}
